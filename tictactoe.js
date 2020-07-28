1a59b4a85e28ee4a2cd39312b75ed8ab22aa812f
Varun <janaki.varun@gmail.com>
2020-07-28 19:38:17 -0400

    Implemented logic 1) to take input from user 2) check if user enters correct position before proceeding

--- tictactoe.js
+++ tictactoe.js
@@ -1,6 +1,7 @@
-var drawBoard = (board) =>{
+const prompt = require('prompt-sync')();
+var drawBoard = (board) => {
     console.log('-------------------')
-    console.log('| ', board.tl, ' | ', board.tm, ' | ', board.tr, ' |' )
+    console.log('| ', board.tl, ' | ', board.tm, ' | ', board.tr, ' |')
     console.log('-------------------')
     console.log('| ', board.ml, ' | ', board.mm, ' | ', board.mr, ' |');
     console.log('-------------------');
@@ -9,70 +10,64 @@ var drawBoard = (board) =>{
 }
 
 var board = {
-    'tl' : ' ',
-    'tm' : ' ',
-    'tr' : ' ',
-    'ml' : ' ',
-    'mm' : ' ',
-    'mr' : ' ',
-    'll' : ' ',
-    'lm' : ' ',
-    'lr' : ' '
+    'tl': ' ',
+    'tm': ' ',
+    'tr': ' ',
+    'ml': ' ',
+    'mm': ' ',
+    'mr': ' ',
+    'll': ' ',
+    'lm': ' ',
+    'lr': ' '
 }
 
-var isGameOver = (board) =>{
-    if(board.tl == board.tm && board.tl == board.tr && board.tl != ' ')
+var isGameOver = (board) => {
+    if (board.tl == board.tm && board.tl == board.tr && board.tl != ' ')
         return true
-    if(board.ml == board.mm && board.ml == board.mr && board.ml != ' ')
+    if (board.ml == board.mm && board.ml == board.mr && board.ml != ' ')
         return true
-    if(board.ll == board.lm && board.ll == board.lr && board.ll != ' ')
+    if (board.ll == board.lm && board.ll == board.lr && board.ll != ' ')
         return true
-    if(board.tl == board.ml && board.tl == board.ll && board.tl != ' ')
+    if (board.tl == board.ml && board.tl == board.ll && board.tl != ' ')
         return true
-    if(board.tm == board.mm && board.tm == board.lm && board.tm != ' ')
+    if (board.tm == board.mm && board.tm == board.lm && board.tm != ' ')
         return true
-    if(board.tr == board.mr && board.tr == board.lr && board.tr != ' ')
+    if (board.tr == board.mr && board.tr == board.lr && board.tr != ' ')
         return true
-    if(board.tl == board.mm && board.tl == board.lr && board.tl != ' ')
+    if (board.tl == board.mm && board.tl == board.lr && board.tl != ' ')
         return true
-    if(board.tr == board.mm && board.tr == board.ll && board.tr != ' ')
+    if (board.tr == board.mm && board.tr == board.ll && board.tr != ' ')
         return true
     return false
 }
 
-var getValueFromUser = () =>{
-    return 'tl'
-}
-
-var getValueFromUser2 = () =>{
-    return 'tr'
-}
-
-
-var getValueFromComputer = () =>{
+var getValueFromComputer = () => {
+    //Yet to write the logic
     return 'tm'
 }
 
-// var uPos = getValueFromUser()
-// board[uPos] = 'x'
-// drawBoard(board)
-// if(!isGameOver(board)){
-//     var cPos = getValueFromComputer() 
-//     board[cPos] = 'x'
-//     drawBoard(board)
-// }
-// if(!isGameOver(board)){
-//     var cPos = getValueFromUser2() 
-//     board[cPos] = 'x'
-//     drawBoard(board)
-// }
-// console.log(isGameOver(board))
+console.log('                      --------Game Started---------                       ');
+console.log('----------------------------------***-------------------------------------');
+console.log('RULE:- Only empty Positions (tl,tm,tr,ml,mm,mr,ll,lm,lr) are to be choosen');
+console.log('----------------------------------***-------------------------------------');
 
-while(!isGameOver()){
-    var uPos = getValueFromUser()
+while (!isGameOver(board)) {
+    do {
+        var uPos = prompt('Select unfilled position');
+        console.log(uPos);
+    } while ( board[uPos] != ' ');
+
     board[uPos] = 'x'
-    if(!isGameOver()){
-        var cPos = getValueFromComputer() 
+    drawBoard(board);
+    if (!isGameOver(board)) {
+        var cPos = getValueFromComputer()
         board[cPos] = 'o'
+        drawBoard(board);
+        if (isGameOver(board)) {
+        console.log('computer won!');
+        }
+    } else {
+        console.log('user won!');
     }
 }
+console.log('--------Game over-----------');
